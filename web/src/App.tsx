97d08c11e85fff5e5389018ffc3742279e988309
Cícero Santos <cicero.dsantos@outlook.com.br>
2023-01-21 10:42:56 -0300

    setup project

---
+++ web/src/App.tsx
@@ -0,0 +1,15 @@
+import { Habit } from "./components/Habit"
+
+import './styles/global.css';
+
+
+function App() {
+
+  return (
+    <>
+      <Habit completed={10}/>
+    </>
+  )
+}
+
+export default App
